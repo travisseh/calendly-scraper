0a2d7497f528060956fa070d4a004fc9998b045b
Travisse Hansen <travisseh@gmail.com>
2024-02-24 23:38:31 -0700

    changed route

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.post('/fetch-calendly', async (req, res) => {
+app.post('/', async (req, res) => {
   const { calendlyUrls } = req.body;
 
   try {
